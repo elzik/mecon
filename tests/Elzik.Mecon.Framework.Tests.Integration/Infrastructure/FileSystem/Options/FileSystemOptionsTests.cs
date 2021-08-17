bdf71c6f41ed072b9ecab3615158131d4930abf2
elzik <justin@elzik.co.uk>
2021-08-17 07:06:32 +0100

    Add options tests

---
+++ tests/Elzik.Mecon.Framework.Tests.Integration/Infrastructure/FileSystem/Options/FileSystemOptionsTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using System.IO;
+using System.Linq;
+using Elzik.Mecon.Framework.Infrastructure.FileSystem.Options;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace Elzik.Mecon.Framework.Tests.Integration.Infrastructure.FileSystem.Options
+{
+    public class PlexOptionsTests
+    {
+        [Fact]
+        public void GetService_ForFileSystemOptions_ReturnsExpectedOptions()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+
+            // Act
+            services.Configure<FileSystemOptions>(configuration.GetSection("FileSystem"));
+            var serviceProvider = services.BuildServiceProvider();
+            var fileSystemOptions = serviceProvider.GetService<IOptions<FileSystemOptions>>();
+
+            // Assert
+            fileSystemOptions.Should().NotBeNull();
+            fileSystemOptions?.Value.Should().NotBeNull();
+            fileSystemOptions?.Value.FolderDefinitions.Should().NotBeNull();
+            fileSystemOptions?.Value.FolderDefinitions.Should().HaveCount(3);
+            AssertFolderDefinitions(fileSystemOptions, 1);
+            AssertFolderDefinitions(fileSystemOptions, 2);
+            AssertFolderDefinitions(fileSystemOptions, 3);
+        }
+
+        private static void AssertFolderDefinitions(IOptions<FileSystemOptions> fileSystemOptions, int i)
+        {
+            fileSystemOptions?.Value.FolderDefinitions.Should().Contain(option =>
+                option.Name == $"test_name_{i}" &&
+                option.FolderPath == $"test_path_{i}" &&
+                option.SupportedFileExtensions.SequenceEqual(new[]
+                {
+                    $"test_ext_{i}_1", $"test_ext_{i}_2", $"test_ext_{i}_3"
+                }));
+        }
+    }
+}
---
+++ tests/Elzik.Mecon.Framework.Tests.Integration/Infrastructure/Plex/Options/PlexOptionsTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using System.IO;
+using Elzik.Mecon.Framework.Infrastructure.Plex.Options;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace Elzik.Mecon.Framework.Tests.Integration.Infrastructure.Plex.Options
+{
+    public class PlexOptionsTests
+    {
+        [Fact]
+        public void GetService_ForPlexOptions_ReturnsExpectedOptions()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // Act
+            services.Configure<PlexOptions>(configuration.GetSection("Plex"));
+            var serviceProvider = services.BuildServiceProvider();
+            var plexOptions = serviceProvider.GetService<IOptions<PlexOptions>>();
+
+            // Assert
+            plexOptions.Should().NotBeNull();
+            plexOptions?.Value.Should().NotBeNull();
+            plexOptions?.Value.AuthToken.Should().Be("test_token");
+            plexOptions?.Value.BaseUrl.Should().Be("test_base_url");
+        }
+    }
+}
