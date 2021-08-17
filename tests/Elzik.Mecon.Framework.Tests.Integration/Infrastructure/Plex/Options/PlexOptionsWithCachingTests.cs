7f960712386224ffa5a7e61761fca0382e72f329
elzik <justin@elzik.co.uk>
2021-08-17 07:11:02 +0100

    Improve naming and add tests for PlexWithCachingOptopns

---
+++ tests/Elzik.Mecon.Framework.Tests.Integration/Infrastructure/Plex/Options/PlexOptionsWithCachingTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using System.IO;
+using Elzik.Mecon.Framework.Infrastructure.Plex.Options;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace Elzik.Mecon.Framework.Tests.Integration.Infrastructure.Plex.Options
+{
+    public class PlexOptionsWithCachingTests
+    {
+        [Fact]
+        public void GetService_ForPlexOptions_ReturnsExpectedOptions()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // Act
+            services.Configure<PlexWithCachingOptions>(configuration.GetSection("Plex"));
+            var serviceProvider = services.BuildServiceProvider();
+            var plexOptions = serviceProvider.GetService<IOptions<PlexWithCachingOptions>>();
+
+            // Assert
+            plexOptions.Should().NotBeNull();
+            plexOptions?.Value.Should().NotBeNull();
+            plexOptions?.Value.AuthToken.Should().Be("test_token");
+            plexOptions?.Value.BaseUrl.Should().Be("test_base_url");
+            plexOptions?.Value.CacheExpiry.Should().Be(3600);
+        }
+    }
+}
