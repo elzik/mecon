ad53ed20c94557b1348d9076d8175472e290eb28
elzik <23397871+elzik@users.noreply.github.com>
2021-08-30 13:38:05 +0100

    Add MediaReconsiler logging tests

---
+++ tests/Elzik.Mecon.Framework.Tests.Unit/Application/MockLogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace Elzik.Mecon.Framework.Tests.Unit.Application
+{
+    public abstract class MockLogger<T> : ILogger<T>
+    {
+        void ILogger.Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+            => Log(logLevel, formatter(state, exception));
+        public abstract void Log(LogLevel logLevel, string message);
+        public virtual bool IsEnabled(LogLevel logLevel) => true;
+        public abstract IDisposable BeginScope<TState>(TState state);
+    }
+}
