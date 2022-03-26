8bd1ba728d6f493222c5d692b809619ea1c3ce4a
Justin Elzik <justin.elzik@activus.co.uk>
2022-03-26 12:47:05 +0000

    Improve console output

--- src/Elzik.Mecon.Console/Program.cs
+++ src/Elzik.Mecon.Console/Program.cs
@@ -16,49 +16,58 @@ using Plex.ServerApi.Clients.Interfaces;
 using FileSystem = Elzik.Mecon.Framework.Infrastructure.FileSystem.FileSystem;
 using IFileSystem = Elzik.Mecon.Framework.Infrastructure.FileSystem.IFileSystem;
 
-var builder = new ConfigurationBuilder();
-builder.AddJsonFile("appsettings.json");
-var configuration = builder.Build();
-
-var version = Assembly.GetExecutingAssembly().GetName().Version;
-if (version == null) throw new InvalidOperationException("It was not possible to get the assembly version.");
-
-var apiOptions = new ClientOptions
+try
 {
-    Product = "mecon",
-    DeviceName = Environment.MachineName,
-    ClientId = "mecon",
-    Platform = Environment.OSVersion.Platform.ToString(),
-    Version = version.ToString()
-};
+    var builder = new ConfigurationBuilder();
+    builder.AddJsonFile("appsettings.json");
+    var configuration = builder.Build();
 
-var services = new ServiceCollection()
-    .AddLogging(loggingBuilder => loggingBuilder.AddConsole())
-    .AddSingleton<IReconciledMedia, MediaReconciler>()
-    .AddTransient<IFileSystem, FileSystem>()
-    .AddTransient<IDirectory, DirectoryWrapper>()
-    .AddTransient<System.IO.Abstractions.IFileSystem, System.IO.Abstractions.FileSystem>()
-    .Configure<FileSystemOptions>(configuration.GetSection("FileSystem"))
-    .Configure<PlexWithCachingOptions>(configuration.GetSection("Plex"))
-    .Configure<PlexOptions>(configuration.GetSection("Plex"))
-    .AddTransient<IPlexEntries, PlexEntries>()
-    .AddSingleton(apiOptions)
-    .AddTransient<IPlexServerClient, PlexServerClient>()
-    .AddTransient<IPlexAccountClient, PlexAccountClient>()
-    .AddTransient<IPlexLibraryClient, PlexLibraryClient>()
-    .AddTransient<IApiService, ApiService>()
-    .AddTransient<IPlexFactory, PlexFactory>()
-    .AddTransient<IPlexRequestsHttpClient, PlexRequestsHttpClient>()
-    .BuildServiceProvider();
+    var version = Assembly.GetExecutingAssembly().GetName().Version;
+    if (version == null) throw new InvalidOperationException("It was not possible to get the assembly version.");
 
-var reconciledMedia = services.GetService<IReconciledMedia>();
-if (reconciledMedia == null) throw new InvalidOperationException("Failed to instantiate ReconciledMedia.");
+    var apiOptions = new ClientOptions
+    {
+        Product = "mecon",
+        DeviceName = Environment.MachineName,
+        ClientId = "mecon",
+        Platform = Environment.OSVersion.Platform.ToString(),
+        Version = version.ToString()
+    };
 
-var entries = await reconciledMedia.GetMediaEntries("Films");
+    var services = new ServiceCollection()
+        .AddLogging(loggingBuilder => loggingBuilder.AddSimpleConsole(options => { options.SingleLine = true; }))
+        .AddSingleton<IReconciledMedia, MediaReconciler>()
+        .AddTransient<IFileSystem, FileSystem>()
+        .AddTransient<IDirectory, DirectoryWrapper>()
+        .AddTransient<System.IO.Abstractions.IFileSystem, System.IO.Abstractions.FileSystem>()
+        .Configure<FileSystemOptions>(configuration.GetSection("FileSystem"))
+        .Configure<PlexWithCachingOptions>(configuration.GetSection("Plex"))
+        .Configure<PlexOptions>(configuration.GetSection("Plex"))
+        .AddTransient<IPlexEntries, PlexEntries>()
+        .AddSingleton(apiOptions)
+        .AddTransient<IPlexServerClient, PlexServerClient>()
+        .AddTransient<IPlexAccountClient, PlexAccountClient>()
+        .AddTransient<IPlexLibraryClient, PlexLibraryClient>()
+        .AddTransient<IApiService, ApiService>()
+        .AddTransient<IPlexFactory, PlexFactory>()
+        .AddTransient<IPlexRequestsHttpClient, PlexRequestsHttpClient>()
+        .BuildServiceProvider();
 
-var badEntries = entries.Where(entry => entry.ReconciledEntries.Count == 0);
+    var reconciledMedia = services.GetService<IReconciledMedia>();
+    if (reconciledMedia == null) throw new InvalidOperationException("Failed to instantiate ReconciledMedia.");
 
-foreach (var badEntry in badEntries)
+    var entries = await reconciledMedia.GetMediaEntries("Films");
+
+    var badEntries = entries.Where(entry => entry.ReconciledEntries.Count == 0);
+
+    foreach (var badEntry in badEntries)
+    {
+        Console.WriteLine(badEntry.FilesystemEntry.FileSystemPath);
+    }
+}
+catch (Exception e)
 {
-    Console.WriteLine(badEntry.FilesystemEntry.FileSystemPath);
+    Environment.ExitCode = 1;
+    Console.Error.WriteLine(e.Message);
+    
 }
