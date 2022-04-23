994cdadd1149293a8a41125aaedf5b3983099395
elzik <23397871+elzik@users.noreply.github.com>
2022-04-23 14:30:33 +0100

    Refactor local publish

--- Local Build/local-publish.ps1
+++ Local Build/local-publish.ps1
@@ -1,23 +1,12 @@
-dotnet publish ..\src\Elzik.Mecon.Console\Elzik.Mecon.Console.csproj `
-	-p:PublishSingleFile=true `
-	-r linux-x64 `
-	-c Release `
-	--self-contained true `
-	-p:PublishTrimmed=true `
-	-o .\output\linux-x64
+$runtimes = "linux-x64", "win-x64", "osx-x64"
 
-dotnet publish ..\src\Elzik.Mecon.Console\Elzik.Mecon.Console.csproj `
-	-p:PublishSingleFile=true `
-	-r win-x64 `
-	-c Release `
-	--self-contained true `
-	-p:PublishTrimmed=true `
-	-o .\output\win-x64
-
+foreach ($runtime in $runtimes)
+{
 	dotnet publish ..\src\Elzik.Mecon.Console\Elzik.Mecon.Console.csproj `
-	-p:PublishSingleFile=true `
-	-r osx-x64 `
-	-c Release `
-	--self-contained true `
-	-p:PublishTrimmed=true `
-	-o .\output\osx-x64
+		-p:PublishSingleFile=true `
+		-r $runtime `
+		-c Release `
+		--self-contained true `
+		-p:PublishTrimmed=true `
+		-o ".\output\$runtime"
+}
