62acb614bf7d61615290fedb615ddc159359d388
elzik <23397871+elzik@users.noreply.github.com>
2022-05-22 13:49:39 +0100

    Ensure command line options are validated before they're used

--- src/Elzik.Mecon.Console/Program.cs
+++ src/Elzik.Mecon.Console/Program.cs
@@ -5,21 +5,19 @@ using Elzik.Mecon.Console.CommandLine.Error;
 using Elzik.Mecon.Console.CommandLine.Reconciliation;
 using Microsoft.Extensions.DependencyInjection;
 
-var config = Configuration.Get(args);
-var services = Services.Get(config);
-
 var commandParser = new Parser(setting =>
 {
     setting.CaseInsensitiveEnumValues = true;
 });
-
 var parserResult = commandParser.ParseArguments<ReconciliationOptions, ConfigOptions>(args);
 
 parserResult
     .WithParsed<ReconciliationOptions>(options =>
     {
+        var config = Configuration.Get(args);
+        var services = Services.Get(config);
         var reconciliationHandler = services.GetRequiredService<IReconciliationHandler>();
         reconciliationHandler.Handle(config, options, args);
     })
-    .WithParsed<ConfigOptions>(_ => ConfigHandler.Display(config))
+    .WithParsed<ConfigOptions>(_ => ConfigHandler.Display(Configuration.Get(args)))
     .WithNotParsed(errors => ErrorHandler.Display(parserResult, errors));
