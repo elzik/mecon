d6d0121af7cf40606918c1124216b0a0a3ffc46a
elzik <23397871+elzik@users.noreply.github.com>
2022-05-24 21:38:14 +0100

    Fix publish paths and ignore output

--- Build/publish.ps1
+++ Build/publish.ps1
@@ -2,11 +2,11 @@ $runtimes = "linux-x64", "win-x64", "osx-x64"
 
 foreach ($runtime in $runtimes)
 {
-	dotnet publish ..\src\Elzik.Mecon.Console\Elzik.Mecon.Console.csproj `
+	dotnet publish $PSScriptRoot\..\src\Elzik.Mecon.Console\Elzik.Mecon.Console.csproj `
 		-p:PublishSingleFile=true `
 		-r $runtime `
 		-c Release `
 		--self-contained true `
 		-p:PublishTrimmed=true `
-		-o ".\output\$runtime"
+		-o $PSScriptRoot\output\$runtime
 }
